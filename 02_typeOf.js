3fb4473e44e66f7a8c2f36a7195843880851b00a
Chetan <chetantheengineer@gmail.com>
2024-03-09 09:25:06 +0530

    Data Type & Type of

---
+++ 02_typeOf.js
@@ -0,0 +1,55 @@
+console.log();
+
+var bankName;
+console.log("1. --------Before Initialization--------");
+console.log();
+console.log("Bank name - " + typeof bankName);
+console.log("Data Type - " + typeof bankName);
+console.log();
+
+bankName = "SBI Bank";
+console.log("--------After Initialization--------");
+console.log();
+console.log("Bank name - " + bankName);
+console.log("Data Type - " + typeof bankName);
+console.log();
+
+console.log("2. ----------------");
+
+var marks = "90%";
+console.log();
+console.log("Marks - " + marks);
+console.log("Data Type - " + typeof marks);
+
+console.log();
+
+console.log("3. --------Before Initialization--------");
+
+var isWorking;
+console.log();
+console.log("isWorking - " + isWorking);
+console.log("Data Type - " + typeof isWorking);
+console.log();
+
+console.log("--------After Initialization--------");
+
+console.log();
+isWorking = true;
+console.log("isWorking - " + isWorking);
+console.log("Data Type - " + typeof isWorking);
+console.log();
+
+
+console.log("4. --------Before--------");
+console.log();
+var totalCount = "Hundred & Seven";
+console.log("Total count - " + totalCount);
+console.log("Data Type - " + typeof totalCount);
+console.log();
+
+console.log("--------After Update--------");
+console.log();
+totalCount = 107;
+console.log("Total count - " + totalCount);
+console.log("Data Type - " + typeof totalCount);
+console.log();
