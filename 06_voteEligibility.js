2a1f31afdea47ef7a0e04820f8c16c1f3e2f654b
Chetan <chetantheengineer@gmail.com>
2024-03-18 10:35:36 +0530

    VoteIfElse

---
+++ 06_voteEligibility.js
@@ -0,0 +1,19 @@
+var voteEligible = function(age){
+    if(age <= 0 || age > 130){
+        console.log(`Invalid data : ${age}`);
+    } else if(age < 18) {
+        console.log(`Not eligible for vote : ${age}`);
+    } else{
+        console.log(`Eligible for vote : ${age}`);
+    }
+}
+
+
+voteEligible(45);
+voteEligible(17);
+voteEligible(8);
+voteEligible(20);
+voteEligible(-10);
+voteEligible(200);
+voteEligible(0);
+voteEligible(null);
